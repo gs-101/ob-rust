7298a934e2bf34d62c4213780f0f18c10c612753
Gabriel Santos <172639817+gs-101@users.noreply.github.com>
2024-12-05 17:47:09 +0000

    Use a temporary ID location file for testing
    Use a temporary file for testing, so it doesn't set the location for the current session.
    
    Closes #12

--- test-ob-rust.el
+++ test-ob-rust.el
@@ -25,7 +25,7 @@
   (expand-file-name (file-name-directory (or load-file-name buffer-file-name))))
 
 (defconst org-id-locations-file
-  (expand-file-name ".test-org-id-locations" ob-rust-test-dir))
+  (org-babel-temp-file "test-org-id-locations-"))
 
 (defun ob-rust-test-update-id-locations ()
   (let ((files (directory-files
